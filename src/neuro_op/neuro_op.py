97d537835d8ca367a1d3919ff1f55751ee06e788
J3nF <108138484+J3nF@users.noreply.github.com>
2023-11-29 19:34:43 +0100

    Cleanup of old notebooks, transition to module import and one notebook per result. Halved N_opinions and belief range to 500 / (-50,50); Started 'PPD-Model-Check' notebook; started KL-divergence implementations for dists and samples as inputs. Tomorrow check whether KL-div works

--- src/neuro_op/neuro_op.py
+++ src/neuro_op/neuro_op.py
@@ -5,6 +5,11 @@ import random
 import scipy.stats as st
 
 
+RANDOM_SEED = np.random.SeedSequence().entropy
+random.seed(RANDOM_SEED)
+rng = np.random.default_rng(RANDOM_SEED)
+
+
 class Agent:
     """
     Agents with belief-holding, -sampling, and -updating behavior.
@@ -35,7 +40,6 @@ class Agent:
         self.diary = np.append(self.diary, incoming_info)
         self.log_probs += self.likelihood.logpdf(x=self.beliefs - incoming_info)
         self.log_probs -= np.max(self.log_probs)  # subtract max for numerical stability
-        # self.likelihood = st.fit(st.norm, logpdf_to_pdf(self.log_probs))
 
     def get_belief_sample(self, size=1):
         """Sample a belief according to world model."""
@@ -43,12 +47,35 @@ class Agent:
         probs = logpdf_to_pdf(self.log_probs)
         sample = np.random.choice(self.beliefs, size=size, p=probs)
         self.protocol = np.append(self.protocol, sample)
+
         return sample
 
     def __repr__(self):
         """Return a string representation of the agent."""
 
         return f"Agent(beliefs={self.beliefs}, log_probs={self.log_probs}, likelihood={self.likelihood}, diary={self.diary}, protocol={self.protocol})"
+
+
+def build_random_network(N_agents, N_neighbours):
+    """
+    Build adjacency matrix of a weighted graph of N_agents, with random connections.
+    At the start, each agent has, on average, 3 connections, each connection is bidirectional and of weight 1.
+    """
+
+    p = N_neighbours / (
+        N_agents - 1
+    )  # probability of connection; '-1' to exclude self-connections
+
+    # size = (N_agents, N_agents)
+    # network = rng.uniform(size=size)
+    # network = np.where(network < p, 1, 0)
+    # network = np.triu(network, k=1)   # remove self-connections
+    # network2 = network + network.T
+    #
+    # return network2
+
+    G = nx.gnp_random_graph(N_agents, p).to_directed()
+    return G  # nx.adjacency_matrix(G).todense()
 
 
 def logpdf_to_pdf(logprobs):
@@ -58,31 +85,58 @@ def logpdf_to_pdf(logprobs):
     Keyword arguments:
     logprobs -- array of log probabilities
     """
-    probs = logprobs - np.max(
-        logprobs
-    )  # shift logprobs to be >0 after exponentiation -- probably needed due to non-normalized log-space Bayesian update
-    probs = np.exp(probs)
+
+    probs = np.exp(
+        logprobs - np.max(logprobs)
+    )  # shift logprobs before np.exp so that exp(logprobs)>0; needed due to non-normalized log-space Bayesian update
+
     return probs / np.sum(probs)
 
 
-def build_random_network(N_agents, N_neighbours):
+def KL_divergence(log_P, log_Q):
     """
-    Build adjacency matrix of a weighted graph of N_agents, with random connections.
-    At the start, each agent has, on average, 3 connections, each connection is bidirectional and of weight 1.
-    """
-    # size = (N_agents, N_agents)
-    p = N_neighbours / (
-        N_agents - 1
-    )  # probability of connection; '-1' to exclude self-connections
-    # network = rng.uniform(size=size)
-    # network = np.where(network < p, 1, 0)
-    # network = np.triu(network, k=1)   # remove self-connections
-    # network2 = network + network.T
-    #
-    # return network2
+    Returns Kullback-Leibler divergence between two arrays of potentially non-normalized log probabilities.
 
-    G = nx.gnp_random_graph(N_agents, p, seed=RANDOM_SEED).to_directed()
-    return G  # nx.adjacency_matrix(G).todense()
+    Keyword arguments:
+    log_P -- array of potentially non-normalized log probabilities of tested distribution
+    log_Q -- array of potentially non-normalized log probabilities of reference distribution
+    """
+
+    # Transform potentially non-normalized log probabilities to normalized probabilities.
+    P = logpdf_to_pdf(log_P)
+    Q = logpdf_to_pdf(log_Q)
+
+    # Return KL-divergence with base 2.
+    return np.sum(P * np.log2(P / Q))
+
+
+def KL_divergence_from_samples(P_samples, Q_samples, N_bins, sample_space_min, sample_space_max):
+    """
+    Returns Kullback-Leibler divergence between samples and a potentially non-normalized log probability distribution.
+
+    Keyword arguments:
+    samples -- array of samples from tested distribution
+    log_Q -- array of potentially non-normalized log probabilities of reference distribution
+    bins -- number of bins used for sample binning
+    sample_space_min -- minimum of sample space interval considered here (usually = belief_min)
+    sample_space_max -- maximum of sample space interval considered here (usually = belief_max)
+    """
+
+    # Get normalized sample probabilites per bin via histogram of samples.
+    P, _ = np.histogram(
+        P_samples,
+        bins=N_bins,
+        range=(sample_space_min, sample_space_max),
+        density=True,
+    )
+    Q, _ = np.histogram(
+        Q_samples,
+        bins=N_bins,
+        range=(sample_space_min, sample_space_max),
+        density=True,
+    )
+
+    return np.sum(P * np.log2(P / Q))
 
 
 def network_dynamics(agents, G, world, h, r, t_end):
@@ -99,15 +153,13 @@ def network_dynamics(agents, G, world, h, r, t_end):
     t_end -- end time of simulation
     """
 
-    assert nx.number_of_nodes(G) == len(agents)
-
     N_nodes = nx.number_of_nodes(G)
     N_edges = nx.number_of_edges(G)
     t = 0
     N_events = 0
 
     while t < t_end:
-        dt = st.expon.rvs(scale=1 / (h + r), random_state=rng)
+        dt = st.expon.rvs(scale=1 / (h + r))
         t = t + dt
         N_events += 1
         event = rng.uniform()
@@ -138,10 +190,10 @@ def network_dynamics(agents, G, world, h, r, t_end):
 def run_model(
     N_agents=100,
     N_neighbours=3,
-    N_beliefs=1000,
-    belief_min=-100,
-    belief_max=100,
-    log_priors=np.zeros(1000),
+    N_beliefs=500,
+    belief_min=-50,
+    belief_max=50,
+    log_priors=np.zeros(500),
     likelihood=st.norm(loc=0, scale=5),
     world_dist=st.norm(loc=0, scale=5),
     h=1,
