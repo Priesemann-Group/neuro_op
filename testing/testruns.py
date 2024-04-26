ec37cc14cf58293936f0326732b71d56998a8b7b
J3nF <108138484+J3nF@users.noreply.github.com>
2024-04-26 18:31:35 +0200

    Found all prerun bugs in either the most or least productive Friday evening ever

--- testing/testruns.py
+++ testing/testruns.py
@@ -7,32 +7,27 @@ import scipy.stats as st
 import time  # runtime measuring
 
 
-def model_runs(input0, dict_list, numero=""):
+def model_run(input0, name=""):
     """
     Call simulate model with 'input0'.
 
     Serially run multiple model parameter sets, safe output to hdf5 file, garbage collect memory.
     """
 
-    for dic_tmp in dict_list:
-        input = copy.deepcopy(input0)
-        adaptions = ""
-        for key, value in dic_tmp.items():
-            input[key] = value
-            adaptions += "--" + str(key) + "-" + str(value)
-        print("Current adaptions:\t", dic_tmp.items())
-        t0 = time.time()
-        output = dict(nop.run_model_Grid(**input))
-        t1 = time.time()
-        print("For adaptions\t", dic_tmp.items(), " :\n\t t_exec = ", (t1 - t0))
-        with open("in" + numero + adaptions + ".pkl", "wb") as f:
-            pickle.dump(input, f)
-        nop.import_hdf5_Grid(output, "out" + numero + adaptions + ".h5")
-        del output
-        gc.collect()
+    input = copy.deepcopy(input0)
+    print("Current adaptions:\t", name)
+    start = time.time()
+    output = dict(nop.run_model_Grid(**input))
+    t1 = time.time()
+    print("\n\t t_exec = ", output["t_exec"], "s\n")
+    with open("in" + name + ".pkl", "wb") as f:
+        pickle.dump(input, f)
+    nop.export_hdf5_Grid(output, "out" + name + ".h5")
+    del output
+    gc.collect()
 
 
-input0 = dict(
+input_ref = dict(
     G=nop.build_random_network(N_nodes=100, N_neighbours=5),  # networkx graph object
     llf_nodes=st.norm,  # Likelihood function (llf) of nodes, Gaussian by default
     llf_world=st.norm,  # Likelihood function (llf) of to-be-approximated world state, Gaussian by default
@@ -54,7 +49,7 @@ input0 = dict(
     h=1,
     r=1,
     t0=0,
-    t_max=20,
+    t_max=50,
     # Sampling parameters...
     t_sample=0.5,
     sample_bins=101,
@@ -64,13 +59,11 @@ input0 = dict(
     progress=False,
 )
 
+input0 = copy.deepcopy(input_ref)
 for numero in range(5):
-    variations = []
     for N in [1.5, 2.5]:
-        G = nop.build_random_network(N_nodes=int(10**N), N_neighbours=5)
-        input0["G"] = G
+        input0["G"] = nop.build_random_network(N_nodes=int(10**N), N_neighbours=5)
         for r_by_h in list(np.linspace(0.0, 1.0, 11)) + list(np.linspace(1.5, 5.0, 9)):
-            variations.append(
-                dict(N_nodes=int(10**N), r=r_by_h)
-            )  # r = r_by_h * h, but h=1 by default
-    model_runs(input0, variations, numero)
+            input0["r"] = r_by_h
+            name = str(numero) + "--N-" + str(N) + "--r-" + str(round(r_by_h, 1))
+            model_run(input0, name)
