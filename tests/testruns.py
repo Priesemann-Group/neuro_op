5faefcf62dd649eda8bc6f7b9416ca41062ebf26
J3nF <108138484+J3nF@users.noreply.github.com>
2024-04-26 17:33:17 +0200

    Added exec time info to output dict, exporting input dict as pickle now. Starting order param. scans with this state & grid run

--- tests/testruns.py
+++ tests/testruns.py
@@ -7,11 +7,11 @@ import scipy.stats as st
 import time  # runtime measuring
 
 
-def model_runs(input0, dict_list):
+def model_runs(input0, dict_list, numero=""):
     """
-    Call 'run_model' with 'input_standard' adapted at specified dictionary entries.
+    Call simulate model with 'input0'.
 
-    Serially run multiple model parameter sets, safe output to pickle file, garbage collect memory.
+    Serially run multiple model parameter sets, safe output to hdf5 file, garbage collect memory.
     """
 
     for dic_tmp in dict_list:
@@ -24,58 +24,53 @@ def model_runs(input0, dict_list):
         t0 = time.time()
         output = dict(nop.run_model(**input))
         t1 = time.time()
-        output["t_start"] = time.strftime("%Y-%m-%d--%H-%M", time.localtime(t0))
-        output["t_exec"] = t1 - t0
-        output["input_dict"] = input
         print("For adaptions\t", dic_tmp.items(), " :\n\t t_exec = ", (t1 - t0))
-        filename = "out" + adaptions + ".pkl"
-        with open(filename, "wb") as f:
-            pickle.dump(output, f)
+        with open("in" + numero + adaptions + ".pkl", "wb") as f:
+            pickle.dump(input, f)
+        nop.import_hdf5_Grid(output, "out" + numero + adaptions + ".h5")
         del output
         gc.collect()
 
 
-input0 = copy.deepcopy(nop.input_standard)
-input0["sample_bins"] = 101
-input0["sample_opinion_range"] = (-50, 50)
+input0 = dict(
+    G=G,  # networkx graph object
+    llf_nodes=st.norm,  # Likelihood function (llf) of nodes, Gaussian by default
+    llf_world=st.norm,  # Likelihood function (llf) of to-be-approximated world state, Gaussian by default
+    params_node=dict(  # Likelihood function (llf) parameters of nodes, Gaussian by default
+        loc=0,
+        scale=1,
+    ),
+    params_world=dict(  # Likelihood function (llf) parameters of to-be-approximated world state, Gaussian by default
+        loc=0,
+        scale=1,
+    ),
+    beliefs=np.linspace(  # beliefs considered by each node
+        start=-10,  # min. considered belief value
+        stop=10,  # max. considered belief value
+        num=500,  # number of considered belief values
+    ),
+    log_priors=np.zeros(500),  # Prior log-probabilities of nodes
+    # Dynamics parameters (rates, simulation times)...
+    h=1,
+    r=1,
+    t0=0,
+    t_max=20,
+    # Sampling parameters...
+    t_sample=0.5,
+    sample_bins=101,
+    sample_range=(-10, 10),
+    p_distance_params=[(1, 1), (2, 1)],
+    # Switches...
+    progress=False,
+)
 
-variations = []
-# Power laws: mean if (a-1)<-2, variance if (a-1)<-3
-# (or: a<-1, a<-2) -- BUT st.powerlaw wants a>0
-for a_tmp in [10, 50, 85]:
-
-    def world_pow(x):
-        return st.powerlaw(a=a_tmp / 100, scale=50).logpdf(x=np.abs(x))
-
+for numero in range(5):
+    variations = []
     for N in [1.5, 2.5]:
-        for r in [0.1, 1, 10]:
-            for op_min in [0, -50]:
-                variations.append(
-                    dict(
-                        N_nodes=int(10**N),
-                        belief_min=op_min,
-                        world_logpdf=world_pow,
-                        r=r,
-                        sample_opinion_range=(
-                            op_min,
-                            input0["sample_opinion_range"][1],
-                        ),
-                    )
-                )
-
-for N in [1.5, 2.5, 3.5]:
-    for r in [0.1, 1, 10]:
-        for prior in [0, 1]:
-            if prior:
-                small_prior = nop.dist_binning(
-                    logpdf=st.norm(loc=0, scale=5).logpdf,
-                    N_bins=len(input0["log_priors"]),
-                    range=input0["sample_opinion_range"],
-
-                )
-                variations.append(dict(N_nodes=int(10**N), log_priors=small_prior, r=r))
-            else:
-                variations.append(dict(N_nodes=int(10**N), r=r))
-
-
-model_runs(input0, variations)
+        G = nop.build_random_network(N_nodes=10**N, N_neighbours=5)
+        input0["G"] = G
+        for r_by_h in list(np.linspace(0.0, 1.0, 11)) + list(np.linspace(1.5, 5.0, 9)):
+            variations.append(
+                dict(N_nodes=int(10**N), r=r_by_h)
+            )  # r = r_by_h * h, but h=1 by default
+    model_runs(input0, variations, numero)
