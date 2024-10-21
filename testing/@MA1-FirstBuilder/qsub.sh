f4798c314cb47065491e8eadb76acf3dae4e132d
J3nF <108138484+J3nF@users.noreply.github.com>
2024-10-21 17:20:45 +0200

    Started final N1,2 analysis, prep. longer run

--- testing/@MA1-FirstBuilder/qsub.sh
+++ testing/@MA1-FirstBuilder/qsub.sh
@@ -47,7 +47,7 @@ mkdir -p $rundir
 cd $rundir
 mkdir -p input log output
 # Execute the above commands
-python ./computation2_N1.py ${SGE_TASK_ID} &
+python ./computation2b_N1.py ${SGE_TASK_ID} &
 # Wait for all the above processes to exit
 wait
 
